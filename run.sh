a9ac109c38f68ab34e5a060714cc2c26408a328a
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2023-01-09 21:21:33 -0800

    add clearing the corpus

--- run.sh
+++ run.sh
@@ -10,6 +10,11 @@ then
 	FUZZFLAG=-O
 fi
 
+if gum confirm "Clear corpus? (This is recommended when switching target)"
+then
+	rm -rf fuzz/artifacts fuzz/corpus
+fi
+
 ROC_SANITIZERS="address,cargo-fuzz" roc build --no-link $ROCFLAG roc_targets/$TARGET
 
 ar rcs roc_targets/libroc-fuzz.a roc_targets/libroc-fuzz.o
