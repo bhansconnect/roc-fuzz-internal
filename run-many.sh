7709a18f27970b08db61ee1fb141220cc4d779a9
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2023-01-23 17:45:06 -0800

    update run-many.sh to print all failures at the end

--- run-many.sh
+++ run-many.sh
@@ -11,9 +11,7 @@ then
 	FUZZFLAG=-O
 fi
 
-# Make sure this exits if anything fails.
-set -e
-
+FAILURES=()
 for TARGET in $TARGETS
 do
 	echo -e "\n\nFuzzing $TARGET\n"
@@ -21,8 +19,28 @@ do
 	rm -rf fuzz/artifacts fuzz/corpus
 
 	ROC_SANITIZERS="address,cargo-fuzz" roc build --no-link $ROCFLAG roc_targets/$TARGET
+	if [ $? -ne 0 ]; then
+		FAILURES+=( $TARGET )
+		continue
+	fi
 
 	ar rcs roc_targets/libroc-fuzz.a roc_targets/libroc-fuzz.o
+	if [ $? -ne 0 ]; then
+		FAILURES+=( $TARGET )
+		continue
+	fi
 
 	cargo fuzz run $FUZZFLAG roc-fuzz -- -max_total_time=$TIMEOUT
+	if [ $? -ne 0 ]; then
+		FAILURES+=( $TARGET )
+		continue
+	fi
+done
+
+echo -e "\n\n==================================="
+echo        "            Failures               "
+echo -e     "===================================\n"
+for FAILURE in ${FAILURES[@]}
+do
+	echo -e "$FAILURE"
 done
